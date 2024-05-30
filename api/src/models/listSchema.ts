5305a6b91921cc9cb6df4cf2ca4093c4461a6538
babanigit <Aniketgithub07>
2024-05-30 12:20:12 +0530

    done with models

---
+++ api/src/models/listSchema.ts
@@ -0,0 +1,26 @@
+import mongoose, { InferSchemaType, Schema } from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        require: true
+    },
+
+    data: {
+        type: String,
+        required: true
+    },
+    done: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+type todo = InferSchemaType<typeof todoSchema>;
+
+const TodoModel = mongoose.model<todo>("todoData", todoSchema)
+export default TodoModel;
