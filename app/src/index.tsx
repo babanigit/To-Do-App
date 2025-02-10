bdb908ae80a13df542eead691c904db68b5680d4
Aniket panchal <aniketpanchal07@gmail.com>
2025-02-10 15:43:44 +0530

    changes in index.tsx

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createRoot } from "react-dom/client"; // Correct import
+import App from "./App"; 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
